0ffc8087571546311c7ff3510c1222ccd5def566
Damian Mullins <damian.mullins@just-eat.com>
2018-03-27 12:34:40 +0100

    Update unit tests.

--- test/Coin.test.js
+++ test/Coin.test.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render, Simulate, flushPromises } from 'react-testing-library';
 import Coin from '../src/components/Coin';
 
-// it('coin markup is correct', () => {
-//   // Arrange
-//   const coin = { name: 'B - Bond... James Bond' };
+it('coin markup is correct', () => {
+  // Arrange
+  const coin = { name: 'B - Bond... James Bond' };
 
-//   // Act
-//   const { container } = render(<Coin coin={coin} />);
+  // Act
+  const { container } = render(<Coin coin={coin} />);
 
-//   // Assert
-//   expect(container.firstChild).toMatchSnapshot();
-// });
+  // Assert
+  expect(container.firstChild).toMatchSnapshot();
+});
 
 it('label text content is correct', () => {
   // Arrange

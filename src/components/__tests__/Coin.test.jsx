ad0abd07ff5cd8d4baf22fc3e23bc1752a2a3e7a
Damian Mullins <damian.mullins@kaluza.com>
2023-07-19 23:32:17 +0100

    v2.0.1 - Fix tests.

---
+++ src/components/__tests__/Coin.test.jsx
@@ -0,0 +1,30 @@
+import { describe, it, expect, afterEach } from 'vitest';
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import Coin from '../Coin';
+
+afterEach(cleanup);
+
+it('coin markup is correct', () => {
+  // Arrange
+  const coin = { id: 2, name: 'Kew Gardens' };
+  const component = <Coin coin={coin} setOwnedValue={() => {}} />;
+
+  // Act
+  const { container } = render(component);
+
+  // Assert
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+it('label text content is correct', () => {
+  // Arrange
+  const coin = { id: 2 };
+  const component = <Coin coin={coin} setOwnedValue={() => {}} />;
+
+  // Act
+  const { getByTestId } = render(component);
+
+  // Assert
+  expect(getByTestId('coin-label').textContent).toBe('');
+});
--- src/lib/__tests__/coinHelper.test.js
+++ src/lib/__tests__/coinHelper.test.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import coinHelper from '../coinHelper';
 
 describe('getDenominations', () => {
